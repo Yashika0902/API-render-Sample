fc283a72bbbfa70a236470bc95df66a9ea94f348
Yashika0902 <sumati0902@gmail.com>
2025-08-27 04:07:49 +0530

    API rendered

---
+++ Sample_two/Screen/UserDetails.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetails.swift
+//  Sample_two
+//
+//  Created by Yashika Kushwaha on 27/08/25.
+//
+
+import SwiftUI
+
+struct UserDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetails()
+}
